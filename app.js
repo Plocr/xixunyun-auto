01bb0e0214bf2cc1c76775630f134c359c16627c
Liu Qiang <114419125+Plocr@users.noreply.github.com>
2023-12-08 17:41:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const signdata = qs.signdata
 const headers = qs.headers
 const loginApi = qs.loginApi
 
-// 签到并提交每日体温报告 
-login().then((token) => {
-  sign(token)
-  studentReportInfo(token)
-})
-
 //登录
 function login() {
   return new Promise((resolve, reject) => {
@@ -37,22 +31,6 @@ function sign(token) {
   })
 }
 
-//日报提交
-function studentReportInfo(token) {
-  const studentReportApi = qs.studentReportApi(token)
-  const studentReportCommitApi = qs.studentReportCommitApi(token)
-  axios.get(studentReportApi).then((res) => {
-    if (res.data.code === 20000) {
-      const { family_name, family_phone } = res.data.data.list[0]
-      const reportForm = qs.reportdata(family_name, family_phone)
-      axios.post(studentReportCommitApi, reportForm).then((res) => {
-        console.log(res.data.code + ',' + res.data.message)
-        wechatSend('习讯云日报提交', res.data.message)
-        // sendEmail('习讯云日报提交', res.data.message)
-      })
-    }
-  })
-}
 
 //推送微信通知
 function wechatSend(type, msg) {
